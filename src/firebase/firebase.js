d50d3168e4d4dfe6640c8c37fbe9713f9d6e8b35
Ankit Kumar <ankitkumar897955@gmail.com>
2025-10-26 16:03:15 +0530

    Update Firebase config to use environment variables
    Replaced hardcoded Firebase configuration with environment variables for better security.

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,15 +2,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyBHqqM6LWkKaSNP9zu8Ddm3SwKf337SRbE",
-//   authDomain: "e-com-4aef5.firebaseapp.com",
-//   projectId: "e-com-4aef5",
-//   storageBucket: "e-com-4aef5.firebasestorage.app",
-//   messagingSenderId: "292196220653",
-//   appId: "1:292196220653:web:b4959ea5bfff8b536956e4"
-// };
-
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
