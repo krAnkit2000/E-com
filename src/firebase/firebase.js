f97096af8b32f98c96146145355ed346ae03d665
Ankit Kumar <ankitkumar897955@gmail.com>
2025-10-26 10:14:37 +0000

    authentication

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,14 +2,14 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBHqqM6LWkKaSNP9zu8Ddm3SwKf337SRbE",
-  authDomain: "e-com-4aef5.firebaseapp.com",
-  projectId: "e-com-4aef5",
-  storageBucket: "e-com-4aef5.firebasestorage.app",
-  messagingSenderId: "292196220653",
-  appId: "1:292196220653:web:b4959ea5bfff8b536956e4"
-};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBHqqM6LWkKaSNP9zu8Ddm3SwKf337SRbE",
+//   authDomain: "e-com-4aef5.firebaseapp.com",
+//   projectId: "e-com-4aef5",
+//   storageBucket: "e-com-4aef5.firebasestorage.app",
+//   messagingSenderId: "292196220653",
+//   appId: "1:292196220653:web:b4959ea5bfff8b536956e4"
+// };
 
 
 const firebaseConfig = {
